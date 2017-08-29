a4cd2403a5a171d6ab6aa6b1b0b1aae83345121b
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2017-08-29 18:15:10 +0530

    Add versioning for command
    By allowing to add version to the YAML, helps to
    create same command for multiple versions of
    product say onap 1.0, 1.1
    
    CLI-37
    Change-Id: Ic3982ea912045ff4f422fcf093cbd98ecd99716d
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

--- framework/src/main/java/org/onap/cli/fw/cmd/OnapSchemaValidateCommand.java
+++ framework/src/main/java/org/onap/cli/fw/cmd/OnapSchemaValidateCommand.java
@@ -29,7 +29,7 @@ import java.util.Map;
 /**
  * Validate schema command.
  */
-@OnapCommandSchema(name = "schema-validate", schema = "schema-validate.yaml")
+@OnapCommandSchema(name = "schema-validate", version="0.0", schema = "schema-validate.yaml")
 public class OnapSchemaValidateCommand extends OnapCommand {
 
     @Override
