ceae19769dad4070c7172c5fed859de0de5172bc
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2017-08-30 17:58:56 +0530

    Add command use
    use command helps to choose the product
    version to use while there are more than
    one product commands present in cli
    
    CLI-37
    Change-Id: I4020bbbcc7574cfcc73ddcd4d46c627087990d20
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

---
+++ framework/src/main/java/org/onap/cli/fw/error/OnapCommandProductVersionInvalid.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.cli.fw.error;
+
+import java.util.Set;
+
+/**
+ * ONAP command product version is invalid
+ *
+ */
+public class OnapCommandProductVersionInvalid extends OnapCommandException {
+
+    private static final long serialVersionUID = 5513297861129088463L;
+
+    public OnapCommandProductVersionInvalid(String invalidVersion, Set<String> validVersions) {
+        super("0x0031", "Given product version " + invalidVersion + " is invalid. Please use one of " + validVersions);
+    }
+}
