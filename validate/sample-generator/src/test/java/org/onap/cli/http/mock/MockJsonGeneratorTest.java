3983a6197ae68d2d9abb2d2358800a2b614015aa
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-10-23 14:03:11 +0000

    Code refactoring to remove test code generation
    Refactor code to remove test code generation code from framework
    codebase.
    
    Issue-ID: CLI-55
    Change-Id: I4b45ef50143317586c39cf118a1717be150707da
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ validate/sample-generator/src/test/java/org/onap/cli/http/mock/MockJsonGeneratorTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.cli.http.mock;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class MockJsonGeneratorTest {
+
+    @Test
+    public void mocoGenerateTest() throws IOException {
+        MockRequest mockRequest = new MockRequest();
+        mockRequest.setJson("{\"value\" : \"234sdf-345\"}");
+        mockRequest.setMethod("get");
+        mockRequest.setUri("http://1.1.1.1:80/getResource");
+
+        MockResponse mockResponse = new MockResponse();
+        mockResponse.setStatus(200);
+        mockResponse.setJson("{\"value\" : \"234sdf-345\"}");
+
+        MockJsonGenerator.generateMocking(mockRequest, mockResponse, "test");
+    }
+
+    @Test(expected=IOException.class)
+    public void mocoGenerateFailedInvalidBodyTest() throws IOException {
+        MockRequest mockRequest = new MockRequest();
+        mockRequest.setJson("{\"value\" : \"234sdf-345\"");
+        mockRequest.setMethod("get");
+        mockRequest.setUri("http://1.1.1.1:80/getResource");
+
+        MockResponse mockResponse = new MockResponse();
+        mockResponse.setStatus(200);
+        mockResponse.setJson("{\"value\" : \"234sdf-345\"");
+
+        MockJsonGenerator.generateMocking(mockRequest, mockResponse, "test");
+    }
+
+    @Test(expected=IOException.class)
+    public void mocoGenerateFailedInvalidUrlTest() throws IOException {
+        MockRequest mockRequest = new MockRequest();
+        mockRequest.setJson("{\"value\" : \"234sdf-345\"");
+        mockRequest.setMethod("get");
+        mockRequest.setUri("http://1.1.1.1:80:invalid");
+
+        MockResponse mockResponse = new MockResponse();
+        mockResponse.setStatus(200);
+        mockResponse.setJson("{\"value\" : \"234sdf-345\"");
+
+        MockJsonGenerator.generateMocking(mockRequest, mockResponse, "test");
+    }
+}
