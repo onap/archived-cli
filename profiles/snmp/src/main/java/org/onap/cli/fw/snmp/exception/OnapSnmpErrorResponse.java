ec214f85f14f2d72acf130190445046968f48089
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-12-21 14:06:08 +0000

    Add snmp profile for cli
    Add snmp profile for cli to support snmp get operation.
    
    Issue-ID: CLI-85
    Change-Id: I7bebd38f2b3089df80c71a5581b23c5408c6d3ab
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ profiles/snmp/src/main/java/org/onap/cli/fw/snmp/exception/OnapSnmpErrorResponse.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.cli.fw.snmp.exception;
+
+import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.error.OnapCommandExecutionFailed;
+
+/**
+ * Command execution failed.
+ *
+ */
+public class OnapSnmpErrorResponse extends OnapCommandExecutionFailed {
+    private static final long serialVersionUID = 488775545433833345L;
+
+    private static final String ERROR_CODE = "0x8001";
+
+    public OnapSnmpErrorResponse(String error, long responseStatus) {
+        super(ERROR_CODE, error, responseStatus);
+    }
+
+    public OnapSnmpErrorResponse(String error) {
+        super(ERROR_CODE, error);
+    }
+
+    public OnapSnmpErrorResponse(Throwable throwable) {
+        super(ERROR_CODE, throwable);
+    }
+
+    public OnapSnmpErrorResponse(Throwable throwable, long responseStatus) {
+        super(ERROR_CODE, throwable, responseStatus);
+    }
+
+}
