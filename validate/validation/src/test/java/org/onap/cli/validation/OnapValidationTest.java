3f92ceaa189fad369a6fc23f788f57e4163d51d5
subhash kumar singh <subhash.kumar.singh@huawei.com>
2018-02-16 13:01:15 +0000

    Impl Verify feature for CLI
    Implement verify feature for CLI. So that command developer
    will come up with his mocking file and can use framework to
    develope the command.
    
    Change-Id: I0ac22aaa7284626de60c66e56e83bb75ec9d773d
    Issue-ID: CLI-74
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- validate/validation/src/test/java/org/onap/cli/validation/OnapValidationTest.java
+++ validate/validation/src/test/java/org/onap/cli/validation/OnapValidationTest.java
@@ -16,27 +16,53 @@
 
 package org.onap.cli.validation;
 
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
 import org.apache.commons.io.FileUtils;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.onap.cli.fw.conf.OnapCommandConstants;
 import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.error.OnapCommandInvalidSample;
 import org.onap.cli.fw.registrar.OnapCommandRegistrar;
 import org.onap.cli.fw.schema.OnapCommandSchemaInfo;
 import org.onap.cli.main.OnapCli;
-import org.onap.cli.moco.OnapCommandHttpMocoServer;
+import org.onap.cli.main.conf.OnapCliConstants;
 import org.onap.cli.moco.OnapCommandSample;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class OnapValidationTest {
 
+    public static final String SAMPLE_VERSION = "open_cli_sample_version";
+    public static final String SAMPLE_VERSION_1_0 = "1.0";
+
+    public static final String SAMPLE_COMMAND_NAME = "name";
+    public static final String SAMPLE_PRODUCT = "version";
+    public static final String SAMPLE_LIST = "samples";
+    public static final String SAMPLE_DESCRIPTION = "name";
+    public static final String SAMPLE_INPUT = "input";
+    public static final String SAMPLE_OUTPUT = "output";
+    public static final String SAMPLE_MOCO = "moco";
+
     OnapCli cli = null;
+
+    private static Logger LOG = LoggerFactory.getLogger(OnapValidationTest.class);
 
     private void handle(String[] args) {
         cli = new OnapCli(args);
@@ -84,13 +110,6 @@ public class OnapValidationTest {
         }
     }
 
-    @Ignore
-    @Test
-    public void verifyCommandResults() throws OnapCommandException {
-        OnapCommandHttpMocoServer server = new OnapCommandHttpMocoServer();
-        server.verifySamples();
-    }
-
     @Test
     public void collectSampleYamlTest() {
         try {
@@ -98,7 +117,7 @@ public class OnapValidationTest {
             String sampleFileName = "target/sample.rst";
 
             FileUtils.deleteQuietly(new File(sampleFileName));
-            Map<String, List<OnapCommandSample>> discoveredYamls = OnapCommandHttpMocoServer.discoverYamls(root);
+            Map<String, List<OnapCommandSample>> discoveredYamls = discoverYamls(root);
 
             writeSamples(new File(sampleFileName), discoveredYamls);
         } catch (IOException e) {
@@ -127,4 +146,89 @@ public class OnapValidationTest {
         }
     }
 
+    public static Map<String, List<OnapCommandSample>> discoverYamls(File path) throws IOException {
+        Map<String, List<OnapCommandSample>> cliProductSamples = new HashMap<>();
+
+        Stream<Path> walk = Files.walk(path.toPath());
+        walk.filter(p -> (p.toString().contains("src/test/resources/onap-cli-sample")))
+                .filter(p -> p.toString().endsWith("sample.yaml"))
+                .forEach(p -> {
+                    collectSamples(new File(p.toUri()), cliProductSamples);
+                });
+
+        return cliProductSamples;
+    }
+
+    private static void collectSamples(File file, Map<String, List<OnapCommandSample>> result) {
+        List<OnapCommandSample> loadSamples;
+        try {
+            loadSamples = loadSamples(file);
+            loadSamples.stream().forEach(sample -> {
+                if (!result.containsKey(sample.getProduct())) {
+                    result.put(sample.getProduct(), new ArrayList<>());
+                }
+                result.get(sample.getProduct()).add(sample);
+            });
+        } catch (OnapCommandInvalidSample e) {
+            LOG.error("Failed to read sample file", e);
+        }
+    }
+
+    private static List<OnapCommandSample> loadSamples(File file) throws OnapCommandInvalidSample {
+        try {
+            return loadSamples(new FileInputStream(file), file.getName());
+        } catch (FileNotFoundException e) {
+            throw new OnapCommandInvalidSample(file.getName(), e);
+        }
+    }
+
+    private static List<OnapCommandSample> loadSamples(InputStream inputStream, String fileName) throws OnapCommandInvalidSample {
+        List<OnapCommandSample> samples = new ArrayList<>();
+        Map<String, ?> values = null;
+        try {
+            values = (Map<String, ?>) new Yaml().load(inputStream);
+        } catch (Exception e) {
+            throw new OnapCommandInvalidSample(fileName, e);
+        }
+
+        OnapCommandSample sample = new OnapCommandSample();
+
+        if (!getValue(values, SAMPLE_VERSION).equals(SAMPLE_VERSION_1_0)) {
+            throw new OnapCommandInvalidSample(fileName, "Invalid sample version " + getValue(values, SAMPLE_VERSION));
+        }
+
+        sample.setCommandName(getValue(values, SAMPLE_COMMAND_NAME));
+        sample.setProduct(getValue(values, SAMPLE_PRODUCT));
+
+        //Retrieve the samples
+        values = (Map<String, Map<String, String>>) values.get(SAMPLE_LIST);
+
+        for (String s: values.keySet()) {
+            Map<String, ?> sMap = (Map<String, ?>)values.get(s);
+            sample.setDescription(getValue(sMap, SAMPLE_DESCRIPTION));
+            sample.setInput(getValue(sMap, SAMPLE_INPUT));
+            sample.setOutput(getValue(sMap, SAMPLE_OUTPUT));
+            sample.setMoco(getValue(sMap, SAMPLE_MOCO));
+            samples.add(sample);
+        }
+
+        return samples;
+    }
+
+    private static String getValue(Map<String, ?> map, String prpName) {
+        Object o = map.get(prpName);
+        if (o != null) {
+            return o.toString();
+        }
+
+        return "";
+    }
+
+    @Test
+    public void testVerify() throws OnapCommandException {
+        OnapCommandRegistrar.getRegistrar().setEnabledProductVersion("open-cli");
+        OnapCli onapCli = new OnapCli(new String[]{"sample-test-verify", "--verify"});
+        onapCli.handle();
+        assertEquals(OnapCliConstants.EXIT_SUCCESS, onapCli.getExitCode());
+    }
  }
