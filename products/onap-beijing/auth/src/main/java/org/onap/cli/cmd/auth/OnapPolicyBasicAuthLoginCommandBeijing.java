b8409c394fa0bda615e961fa916b4f8e784eb654
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2018-04-24 18:14:30 +0530

    Fix policy login plugin to avoid run overrides
    Issue-ID: CLI-100
    
    Change-Id: Iaf448786de77d8cdc3fc6fa40aec7c002b091a6d
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

--- products/onap-beijing/auth/src/main/java/org/onap/cli/cmd/auth/OnapPolicyBasicAuthLoginCommandBeijing.java
+++ products/onap-beijing/auth/src/main/java/org/onap/cli/cmd/auth/OnapPolicyBasicAuthLoginCommandBeijing.java
@@ -16,15 +16,9 @@
 
 package org.onap.cli.cmd.auth;
 
-import org.onap.cli.fw.error.OnapCommandException;
 import org.onap.cli.fw.http.cmd.BasicAuthLoginCommand;
 import org.onap.cli.fw.schema.OnapCommandSchema;
 
 @OnapCommandSchema(schema = "basic-login-onap-policy-beijing.yaml")
 public class OnapPolicyBasicAuthLoginCommandBeijing extends BasicAuthLoginCommand {
-
-    @Override
-    protected void run() throws OnapCommandException {
-        //don't do anything...
-    }
 }
