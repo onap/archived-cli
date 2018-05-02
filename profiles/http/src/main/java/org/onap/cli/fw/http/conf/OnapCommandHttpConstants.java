d7aa562decb865691eda0e5b90d113ce6d1aa812
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2018-05-02 14:48:31 +0530

    Fix the HEAD name in http profile
    Issue-ID: CLI-106
    
    Change-Id: Ie18578b1b284283c889a7584e6dd6795be55489a
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

--- profiles/http/src/main/java/org/onap/cli/fw/http/conf/OnapCommandHttpConstants.java
+++ profiles/http/src/main/java/org/onap/cli/fw/http/conf/OnapCommandHttpConstants.java
@@ -58,7 +58,7 @@ public class OnapCommandHttpConstants {
     public static final String GET = "get";
     public static final String DELETE = "delete";
     public static final String PUT = "put";
-    public static final String HEAD = "delete";
+    public static final String HEAD = "head";
     public static final String HEADERS = "headers";
     public static final String QUERIES = "queries";
     public static final String COOKIES = "cookies";
