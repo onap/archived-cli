673668dcfb5ba09da6b4b654a39ec169a5538eff
jitendra sharma <jitendra.sharma1@huawei.com>
2019-08-08 10:44:36 +0000

    UT for OnapCommandExecutionStore UT for storing of execution details such as output,debug. Issue-ID: CLI-169
    Signed-off-by: jitendra sharma <jitendra.sharma1@huawei.com>
    Change-Id: Ia281273204360f17225214898521f8810e702cf4

---
+++ framework/src/test/java/org/onap/cli/fw/store/OnapCommandExecutionStoreTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2019 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onap.cli.fw.store;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.cli.fw.error.OnapCommandExecutionFailed;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class OnapCommandExecutionStoreTest {
+    OnapCommandExecutionStore executionStore;
+    @Before
+    public void setUp() throws Exception {
+        executionStore=OnapCommandExecutionStore.getStore();
+    }
+    @Test
+    public void storeExectutionStartTest(){
+        assertNotNull(executionStore.storeExectutionStart("requestId","product","service","cmd","profile","input"));
+    }
+    @Test
+    public void listExecutionsTest() throws OnapCommandExecutionFailed {
+        Map<String,String> search=new HashMap<>();
+        search.put("startTime","12");
+        search.put("endTime","12");
+        search.put("execution-id","abc");
+        search.put("request-id","abc");
+        assertNotNull(executionStore.listExecutions(search));
+    }
+    @Test
+    public void storeExectutionEndTest(){
+        OnapCommandExecutionStore.ExecutionStoreContext store=new OnapCommandExecutionStore.ExecutionStoreContext();
+        store.setExecutionId("abc");
+        store.setRequestId("abc");
+        store.setStorePath("abc");
+        executionStore.storeExectutionEnd(store,"abc","abc","abc",true);
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"abc").exists());
+    }
+    @Test
+    public void storeExectutionProgressTest(){
+        OnapCommandExecutionStore.ExecutionStoreContext store=new OnapCommandExecutionStore.ExecutionStoreContext();
+        store.setExecutionId("abc");
+        store.setRequestId("abc");
+        store.setStorePath("abc");
+        executionStore.storeExectutionProgress(store,"abc","abc","abc");
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"abc").exists());
+    }
+    @Test
+    public void storeExectutionDebugTest(){
+        OnapCommandExecutionStore.ExecutionStoreContext store=new OnapCommandExecutionStore.ExecutionStoreContext();
+        store.setExecutionId("abc");
+        store.setRequestId("abc");
+        store.setStorePath("abc");
+        executionStore.storeExectutionDebug(store,"abc");
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"abc").exists());
+    }
+    @Test
+    public void storeExectutionOutputTest(){
+        OnapCommandExecutionStore.ExecutionStoreContext store=new OnapCommandExecutionStore.ExecutionStoreContext();
+        store.setExecutionId("abc");
+        store.setRequestId("abc");
+        store.setStorePath("abc");
+        executionStore.storeExectutionOutput(store,"abc");
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"abc").exists());
+    }
+
+}
