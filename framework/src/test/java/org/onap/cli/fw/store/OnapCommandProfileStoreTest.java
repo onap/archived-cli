0f1e646d69135777bbabc9f15f4ce2fa8beb30e3
jitendra sharma <jitendra.sharma1@huawei.com>
2019-08-08 10:52:38 +0000

    UT for OnapCommandProfileStoreTest UT for profilestore create, remove and update
    Issue-ID: CLI-169
    
    Signed-off-by: jitendra sharma <jitendra.sharma1@huawei.com>
    Change-Id: Ibe1fa94692584d11d3d35155a69049045b5c0c55

---
+++ framework/src/test/java/org/onap/cli/fw/store/OnapCommandProfileStoreTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.cli.fw.store;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.error.OnapCommandPersistProfileFailed;
+import org.onap.cli.fw.input.cache.OnapCommandParamEntity;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OnapCommandProfileStoreTest {
+    OnapCommandProfileStore onapCommandProfileStore;
+    @Before
+    public void setUp() throws Exception {
+        onapCommandProfileStore=OnapCommandProfileStore.getInstance();
+    }
+    @Test
+    public void includeProfileTest() throws OnapCommandException {
+        onapCommandProfileStore.includeProfile("profiles");
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"data/profiles").exists());
+    }
+    @Test
+    public void persistProfileAndgetProfilesTest() throws OnapCommandPersistProfileFailed {
+        OnapCommandParamEntity onapCommandParamEntity=new OnapCommandParamEntity();
+        onapCommandParamEntity.setName("schema-list");
+        onapCommandParamEntity.setProduct("open-cli");
+        onapCommandParamEntity.setValue("value");
+        List<OnapCommandParamEntity> paramEntityList=new ArrayList<>();
+        paramEntityList.add(onapCommandParamEntity);
+        onapCommandProfileStore.persistProfile(paramEntityList,"abc");
+        assertTrue(new File(System.getProperty("user.dir")+File.separator+"data/profiles/abc-profile.json").exists());
+        assertNotNull(onapCommandProfileStore.getProfiles());
+    }
+    @Test
+    public void removeProfileTest(){
+        onapCommandProfileStore.removeProfile("abc");
+        assertFalse(new File(System.getProperty("user.dir")+File.separator+"data/profiles/abc-profile.json").exists());
+    }
+    @Test
+    public void addTest(){
+        onapCommandProfileStore.add("abc","abc","abc");
+    }
+@Test
+    public void getParamsTest(){
+        assertNotNull(onapCommandProfileStore.getParams("abc"));
+}
+
+}
