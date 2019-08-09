80ee349e47dddb95cc782a2b87e3ca9a9e3da4eb
jitendra sharma <jitendra.sharma1@huawei.com>
2019-08-09 08:36:34 +0000

    UT for Onap Commands execution-show and execution-show-debug UT Classes are OnapCommandExceutionShowCommandTest and  OnapCommandExceutionShowDebugCommandTest
    Issue-ID: CLI-169
    
    Signed-off-by: jitendra sharma <jitendra.sharma1@huawei.com>
    Change-Id: I812df7dd504a72f1fed33fbbdb2781f4fa22fc3c

---
+++ framework/src/test/java/org/onap/cli/fw/cmd/execution/OnapCommandExceutionShowCommandTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.cli.fw.cmd.execution;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.error.OnapCommandExecutionFailed;
+import org.onap.cli.fw.output.OnapCommandResultAttribute;
+import org.onap.cli.fw.store.OnapCommandExecutionStoreTest;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class OnapCommandExceutionShowCommandTest {
+@BeforeClass
+public static void setUp() throws Exception {
+    OnapCommandExecutionStoreTest executionStoreTest= new OnapCommandExecutionStoreTest();
+    executionStoreTest.setUp();
+    executionStoreTest.listExecutionsTest();
+    executionStoreTest.storeExectutionDebugTest();
+    executionStoreTest.storeExectutionEndTest();
+    executionStoreTest.storeExectutionOutputTest();
+    executionStoreTest.storeExectutionProgressTest();
+    executionStoreTest.storeExectutionStartTest();
+}
+    @Test
+    public void runTest() throws OnapCommandException {
+        OnapCommandExceutionShowCommand cmd=new OnapCommandExceutionShowCommand();
+        cmd.initializeSchema("execution-show.yaml");
+        cmd.getParametersMap().get("execution-id").setValue("requestId");
+        cmd.getParametersMap().get("format").setValue("TEXT");
+        cmd.execute();
+        List<OnapCommandResultAttribute> oclipCommandResultAttributes = cmd.getResult()
+                .getRecords();
+        assertTrue(oclipCommandResultAttributes.size() > 1);
+    }
+}
