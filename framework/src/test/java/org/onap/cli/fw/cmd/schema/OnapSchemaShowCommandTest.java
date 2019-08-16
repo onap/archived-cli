ebfe71959dc6243ac30b9fe39b84b96b73718ce9
jitendra sharma <jitendra.sharma1@huawei.com>
2019-08-16 06:17:15 +0000

    add Junit test case wth cleanup code for OnapSchemaShowCommandTest
    Issue-ID: CLI-169
    
    Signed-off-by: jitendra sharma <jitendra.sharma1@huawei.com>
    Change-Id: I46fc8bb4a351c2767fea8fd4ba88287b31a7086e

---
+++ framework/src/test/java/org/onap/cli/fw/cmd/schema/OnapSchemaShowCommandTest.java
@@ -0,0 +1,57 @@
+ /*
+  * Copyright 2019 Huawei Technologies Co., Ltd.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+
+ package org.onap.cli.fw.cmd.schema;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.onap.cli.fw.cmd.execution.OnapCommandExceutionListCommandTest;
+import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.output.OnapCommandResultAttribute;
+import org.onap.cli.fw.store.OnapCommandExecutionStoreTest;
+import org.onap.cli.fw.utils.FileUtil;
+
+import java.io.File;
+import java.util.List;
+import static org.junit.Assert.*;
+
+public class OnapSchemaShowCommandTest {
+    static OnapCommandExecutionStoreTest executionStoreTest;
+    @BeforeClass
+    public static void setUp() throws Exception {
+        executionStoreTest= new OnapCommandExecutionStoreTest();
+        executionStoreTest.setUp();
+        executionStoreTest.storeExectutionStartTest();
+    }
+    @Test
+    public void runTest() throws OnapCommandException {
+        OnapSchemaShowCommand cmd = new OnapSchemaShowCommand ();
+        cmd.initializeSchema("schema-show.yaml");
+        cmd.getParametersMap().get("product").setValue("open-cli");
+        cmd.getParametersMap().get("command").setValue("schema-show");
+        cmd.execute();
+        List<OnapCommandResultAttribute> oclipCommandResultAttributes = cmd.getResult()
+                .getRecords();
+        assertTrue(oclipCommandResultAttributes.size()>0);
+    }
+ @AfterClass
+    public static void tearDown() throws Exception {
+        String dirPathForExecutions = System.getProperty("user.dir") + File.separator + "data/executions";
+        File executionsFile = new File(dirPathForExecutions);
+        assertTrue(OnapCommandExceutionListCommandTest.deleteDirectory(executionsFile));
+    }
+    }
