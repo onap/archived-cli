3d6087187079959ff9baa205ba7f0516681ce4b8
jitendra sharma <jitendra.sharma1@huawei.com>
2019-08-16 05:28:16 +0000

    add Junit test case wth cleanup code for OnapCommandExceutionShowOutCommandTest
    Issue-ID: CLI-169
    
    Signed-off-by: jitendra sharma <jitendra.sharma1@huawei.com>
    Change-Id: If82e6f4e7fe9208581c264396c4d260adb304ab8

---
+++ framework/src/test/java/org/onap/cli/fw/cmd/execution/OnapCommandExceutionShowOutCommandTest.java
@@ -0,0 +1,53 @@
+/*
+  * Copyright 2019 Huawei Technologies Co., Ltd.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *     http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package org.onap.cli.fw.cmd.execution;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.onap.cli.fw.error.OnapCommandException;
+import org.onap.cli.fw.store.OnapCommandExecutionStoreTest;
+import org.onap.cli.fw.utils.FileUtil;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class OnapCommandExceutionShowOutCommandTest {
+    static OnapCommandExecutionStoreTest executionStoreTest;
+    @BeforeClass
+    public static void setUp() throws Exception {
+        executionStoreTest= new OnapCommandExecutionStoreTest();
+        executionStoreTest.setUp();
+        executionStoreTest.storeExectutionStartTest();
+    }
+    @Test
+    public void runTest() throws OnapCommandException {
+        OnapCommandExceutionShowOutCommand cmd=new OnapCommandExceutionShowOutCommand();
+        cmd.initializeSchema("execution-show-out.yaml");
+        cmd.getParametersMap().get("execution-id").setValue("requestId");
+        cmd.execute();
+        assertNotNull(cmd.getResult().getOutput());
+
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        String dirPathForExecutions = System.getProperty("user.dir") + File.separator + "data/executions";
+        File executionsDir = new File(dirPathForExecutions);
+        assertTrue(OnapCommandExceutionListCommandTest.deleteDirectory(executionsDir));
+    }
+}
