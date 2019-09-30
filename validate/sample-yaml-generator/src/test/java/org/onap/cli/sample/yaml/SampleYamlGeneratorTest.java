394f7b6c07dcd6cad153703a9fe5c9ebae9488d4
Kanagaraj M <mkr1481@gmail.com>
2019-09-30 06:19:30 +0200

    Fix ovp lcm validation
    Issue-ID: CLI-166
    
    Change-Id: I0c8def49c6d3eb79d4686b40899ff165ae2461ce
    Signed-off-by: Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>

--- validate/sample-yaml-generator/src/test/java/org/onap/cli/sample/yaml/SampleYamlGeneratorTest.java
+++ validate/sample-yaml-generator/src/test/java/org/onap/cli/sample/yaml/SampleYamlGeneratorTest.java
@@ -26,7 +26,7 @@ public class SampleYamlGeneratorTest {
     @Test
     public void testGenerateSampleYaml() throws IOException {
         SampleYamlGenerator.generateSampleYaml("testcmd", Arrays.asList("-a", "argument"),
-                "+--------+\n+val     +\n+argument+", "test-version-1.0", "target/test.yaml", false);
+                "+--------+\n+val     +\n+argument+", "test-version-1.0", "target/test.yaml", false, "sample1");
     }
 
 }
