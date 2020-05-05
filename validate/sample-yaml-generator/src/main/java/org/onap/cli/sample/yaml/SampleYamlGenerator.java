b728dc08a8893e3d39fdbf31a33758de6bae3771
priyanka.akhade <priyanka.akhade@huawei.com>
2020-05-05 09:36:31 +0000

    sonar bug fix- conditional operation returns the same value whether the condition is "true" or "false"
    Signed-off-by: priyanka.akhade <priyanka.akhade@huawei.com>
    Issue-ID: CLI-270
    Change-Id: I500b47559bba7b8cf366f70357041c4c516b8f90

--- validate/sample-yaml-generator/src/main/java/org/onap/cli/sample/yaml/SampleYamlGenerator.java
+++ validate/sample-yaml-generator/src/main/java/org/onap/cli/sample/yaml/SampleYamlGenerator.java
@@ -61,7 +61,7 @@ public class SampleYamlGenerator {
         writer.write(" |\n");
         nTab++;
         String[] lines = value.split("\n");
-        long skipLines = debug ? 0 : 0;
+        long skipLines = 0;
         Arrays.stream(lines).skip(skipLines ).forEach(line -> writer.write(printTabs() + line + "\n")); // NOSONAR
     }
 
