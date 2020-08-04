0508dfbe56d834b26b98d06011cd7f26cf734b98
priyanka.akhade <priyanka.akhade@huawei.com>
2020-08-04 19:07:52 +0530

    Code improvement - Pending sonar issues
    Signed-off-by: priyanka.akhade <priyanka.akhade@huawei.com>
    Issue-ID: CLI-270
    Change-Id: I11bff3e6fa5f3299ee3ad0f7bf5d3e42ee7a704c

--- framework/src/main/java/org/onap/cli/fw/error/OnapCommandTimeOutException.java
+++ framework/src/main/java/org/onap/cli/fw/error/OnapCommandTimeOutException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Huawei Technologies Co., Ltd.
+ * Copyright 2020 Huawei Technologies Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,23 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package org.onap.cli.fw.error;
 
-package org.onap.cli.fw.cmd.dummy;
+public class OnapCommandTimeOutException extends RuntimeException {
+    private static final long serialVersionUID = -4280521008892487435L;
 
-import org.onap.cli.fw.cmd.OnapCommand;
-import org.onap.cli.fw.error.OnapCommandException;
-import org.onap.cli.fw.schema.OnapCommandSchema;
-
-/**
- * List available schemas.
- *
- */
-@OnapCommandSchema(type = "dummy")
-public class OnapCommandDummy extends OnapCommand {
-
-    @Override
-    protected void run() throws OnapCommandException {
-        // Dummy Method
+    public OnapCommandTimeOutException(String message) {
+        super(message);
     }
-
 }
